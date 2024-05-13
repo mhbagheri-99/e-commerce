8553cd1e357a3dfe75439cd858970096e629c3d9
mhbagheri-99 <mhbagheri3@gmail.com>
2024-05-13 15:45:09 +0200

    initial stripe setup and checkout function

--- src/app/(customerSide)/products/[id]/purchase/page.tsx
+++ src/app/(customerSide)/products/[id]/purchase/page.tsx
@@ -1,4 +1,9 @@
 import db from "@/db/db";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+import CheckoutForm from "./_components/CheckoutForm";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 const ProductPurchasePage = async ({
   params: { id },
@@ -8,7 +13,28 @@ const ProductPurchasePage = async ({
   const product = await db.product.findUnique({
     where: { id },
   });
-  return <div>page</div>;
+  if (!product) {
+    return notFound();
+  }
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: {
+      product: product.id,
+    },
+  });
+
+  if (!paymentIntent.client_secret) {
+    throw new Error("Stripe failed to create payment intent properly");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 };
 
 export default ProductPurchasePage;
